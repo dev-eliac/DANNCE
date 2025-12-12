f553675e12148f7bea18b9a0852e2afbc1339825
dev-eliac <devilyaskhan7139@gmail.com>
2025-12-12 16:06:56 +0500

    reduced batch size and increased adv steps

--- run_sketch.sh
+++ run_sketch.sh
@@ -14,10 +14,9 @@ python3 -m src.main \
     --gpu=0 \
     --random_seed=$random_seed \
     --adversarial_examples_lr=1e-3 \
-    --adversarial_train_steps=3 \
+    --adversarial_train_steps=5 \
     --adversarial_examples_wd=1e-3 \
     --adversarial_examples_ratio=0.5 \
-    --batch_size=1024 \
     --adv_blur_step=4 \
     --adv_kl_weight=1 \
     --single_target=sketch \
